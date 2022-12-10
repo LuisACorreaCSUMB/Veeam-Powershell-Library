71a90c2cf1074cf63a162084aa45270325847d83
LuisACorreaCSUMB <lucorrea@csumb.edu>
2022-12-10 06:02:30 -0800

    Additional Documentation
    Cleansed Documentation.
    Repaired the 365 setup Full organization script

--- 365/Setup/JobCreationFullOrganization.ps1
+++ 365/Setup/JobCreationFullOrganization.ps1
@@ -1,7 +1,9 @@
 #Veeam 365 Job Creation for the entire Organization
-$Organization = Read-Host "Please enter the organization name"
+$OrganizationName = Read-Host "Please enter the organization name"
+$Org = Get-VBOOrganization -Name $OrganizationName
 $JobName = Read-Host "Please enter in the Job Name"
-$Repository = Read-Host "Please enter in the Repository Name"
-Add-VBOJob -Organization $Organization -Name $JobName -Repository $Repository -EntireOrganization
+$RepositoryName = Read-Host "Please enter in the Repository Name"
+$Repo = Get-VBORepository -Name $RepositoryName
+Add-VBOJob -Organization $Org -Name $JobName -Repository $Repo -EntireOrganization
 Write-Output 'Script ran Successfully.'
 cmd /c pause
