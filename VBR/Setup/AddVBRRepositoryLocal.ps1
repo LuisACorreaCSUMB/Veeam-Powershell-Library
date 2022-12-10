359c9f790af5d70efe360054951f9f5ceac49797
LuisACorreaCSUMB <lucorrea@csumb.edu>
2022-12-10 14:00:33 -0800

    Commiting scripts for testing
    Commiting to dev branch 2 more scripts to test on Dev environment to confirm before writing the documentation

---
+++ VBR/Setup/AddVBRRepositoryLocal.ps1
@@ -0,0 +1,8 @@
+﻿#Script to add Repository that doesn't require Credentials
+$Path = Read-Host "Please enter the file path of the repository"
+$ServerName = Read-Host "Please enter the name of the server where the repository will be located"
+$Name = Read-Host "Please enter in the name of the new repository"
+$Description = Read-Host "Please enter in the Description"
+$Server = Get-VBRServer -Name $ServerName
+Add-VBRBackupRepository -Folder $Path -Type WinLocal -Name $Name -Description $Description -Server $Server
+cmd /c pause
